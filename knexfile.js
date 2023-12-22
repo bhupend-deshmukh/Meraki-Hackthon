39d39696cc063eb5743d29787900979df98257fc
bhupend-deshmukh <bhupendradeshmukh2018@gmail.com>
2023-12-22 17:08:02 +0530

    connection string

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      host:DB_HOST,
-      user:DB_USER,
-      password:DB_PASS,
-      database:DB_NAME,
-
-    }
+    connection: process.env.DB_STRING
   },
 
   staging: {
