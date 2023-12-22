292683173fac03535fa24b19323aaaae4d2ff86f
bhupend-deshmukh <bhupendradeshmukh2018@gmail.com>
2023-12-22 08:30:34 +0530

    ..

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // create server 
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // import routes
 const userRouter = require('./routes/users.routes');
