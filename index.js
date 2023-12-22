0db7fe253c9694c8d006368806824cdc2b1e6c11
bhupend-deshmukh <bhupendradeshmukh2018@gmail.com>
2023-12-22 08:15:41 +0530

    change environment

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const userRouter = require('./routes/users.routes');
 app.use(express.json());
 app.use('/api',userRouter)
 
+// get api cwrite
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
 // listen port
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,16 @@
 const Dotenv = require("dotenv").config();
-const { DB_NAME, DB_USER, DB_PASS,} = process.env;
+const { DB_NAME, DB_USER, DB_PASS,DB_HOST} = process.env;
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: {
+      host:DB_HOST,
       user:DB_USER,
       password:DB_PASS,
-      database:DB_NAME
+      database:DB_NAME,
+
     }
   },
 
